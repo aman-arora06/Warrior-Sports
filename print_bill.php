8a3d40d5b09501c1eb11e9770f6cf42a80550efa
sayambothra <63236442+sayambothra@users.noreply.github.com>
2020-11-27 07:09:22 -0500

    Add files via upload

---
+++ print_bill.php
@@ -0,0 +1,89 @@
+<?php
+
+
+$conn=mysqli_connect('localhost','root','Champion1@','warriorsports');
+
+if(!$conn){
+echo 'connection error';
+}
+
+
+$sql="SELECT Serial_no,Title,Name,Stadium,Sports,phone1,phone2,RacquetBrand,RacquetModel,
+StringType,StringTension,StringColor,RacquetCover,
+CollectionDate,ExpDeliveryDate,TotalAmount FROM Informative WHERE Serial_no='89'";
+$result = $conn->query($sql);
+?>
+<style>
+bode{
+  width:8.5in;
+  margin:0in .1875in;
+
+}
+@media print {
+body *{
+ visibility:hidden;
+}
+@page{
+  size:3in 1.5in;
+  size:portrait;
+}
+#printable, #printable *{
+ visibility: visible;
+}
+#printable {
+ width:3in;
+ height:1.5in;
+ margin-top:0px;
+ margin-right:.125in;
+ overflow: hidden;
+}
+
+.page-break{
+  display:block; page-break-after:always;
+}
+}
+</style>
+<body>
+<div align="center" id="Printable">
+
+    <table border style ="border-top-width:0px;border-left-width:0px;border-right-width:0px;border-bottom-width:0px;">
+
+        <th style="text-align:center;border-top-width:0px;border-left-width:0px;border-right-width:0px;">
+          <b style="font-size:20px;">World of Warriors</b></th>
+          <?php
+          while($row = $result->fetch_assoc()) {
+            ?>
+
+        <?php
+        $cdate=$row['CollectionDate'];
+        $c_date=explode(" ",$cdate);
+        $newdate=date("d/m/Y",strtotime($c_date[0]));
+        $ED=$row['ExpDeliveryDate'];
+        $EOD=date("d/m/Y",strtotime($ED));
+        $Rbrand=$row['RacquetBrand'];
+        $NRbrand=explode(",",$Rbrand);
+        $Rlen=count($NRbrand);
+        ?>
+        <!-- // echo $Rlen;
+        // if($Rlen>1){
+        // for($i=0;$i<$Rlen;$i++){ -->
+         <tr><td style=font-size:10px;border-top-width:0px;border-left-width:0px;border-right-width:0px;border-bottom-width:0px;><?php echo "DATE OF RECEIPT: ".$newdate." TX:".$row['Serial_no']?></td></tr>
+        <tr><td style=font-size:10px;border-top-width:0px;border-left-width:0px;border-right-width:0px;border-bottom-width:0px;><?php echo "NAME: ".$row['Name']."    TEL: ".$row['phone1']?></td></tr>
+        <tr><td style=font-size:10px;border-top-width:0px;border-left-width:0px;border-right-width:0px;border-bottom-width:0px;><?php echo "CLUB: ".$row['Stadium']?></td><tr>
+        <tr><td style=font-size:10px;border-top-width:0px;border-left-width:0px;border-right-width:0px;border-bottom-width:0px;><?php echo "RACQUET DETAILS: ".$row['Sports']." ".$row['RacquetBrand']." ".$row['RacquetModel']?></td><tr>
+        <tr><td style=font-size:10px;border-top-width:0px;border-left-width:0px;border-right-width:0px;border-bottom-width:0px;><?php echo"STRING/TENSION: ".$row['StringType']."/".$row['StringTension']."LBS"?></td><tr>
+       <tr><td style=font-size:10px;border-top-width:0px;border-left-width:0px;border-right-width:0px;border-bottom-width:0px;><?php echo "EXP DELIVERY DATE: ".$EOD." Cover: ".$row['RacquetCover']?></td><tr>
+        <tr><td style=font-size:10px;border-top-width:0px;border-left-width:0px;border-right-width:0px;border-bottom-width:0px;><?php echo"BILL: ".$row['TotalAmount']?></td><tr>
+        <tr><td style=font-size:10px;border-top-width:0px;border-left-width:0px;border-right-width:0px;border-bottom-width:0px;><?php echo"Powered by: WARRIOR SPORTS(INDIA) +91 -80776683186/9582704145"?></td></tr>
+
+
+
+        <?php } ?>
+
+</table>
+
+
+</div>
+<div class="page-break"></div>
+</body>
+<input type="button" onclick="window.print()"value="print now">
